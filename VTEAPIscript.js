c18847e45d2d021e78c138e2d2fcc8ddde094081
drhaseeb <109030802+drhaseeb@users.noreply.github.com>
2024-02-23 17:46:07 +0500

    Create VTEAPIscript.js

---
+++ VTEAPIscript.js
@@ -0,0 +1,33 @@
+// Your client ID and API key from the Google Cloud console
+var CLIENT_ID = '678218067433-lt9r3i0pfdrbdh165c2d7isoial08422.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyDdXpNR_fkUK2YYz4zpdcL0_thO1bGCFDU';
+
+// The scopes you need to access the Google Sheets API
+var SCOPES = 'https://www.googleapis.com/auth/spreadsheets';
+
+// The ID of the spreadsheet you want to access
+var SPREADSHEET_ID = '1npHjzp9Y8RxJhsb-ehdRrE9fIWLguzoH6WYaAYcThwo';
+
+// Initialize the Google Identity Services client
+var client = google.accounts.oauth2.initTokenClient({
+	client_id: CLIENT_ID,
+	scope: SCOPES,
+	callback: (tokenResponse) => {
+		// The user is signed in, you can make API calls here
+		// For example, you can get the OAuth token and send it with your request
+		var token = tokenResponse.access_token;
+		console.log('Token: ' + token);
+	},
+});
+
+// Request an access token from the user
+function getToken() {
+	client.requestAccessToken();
+}
+
+// Revoke the access token from the user
+function revokeToken() {
+	google.accounts.oauth2.revoke(token, () => {
+		console.log('Token revoked');
+	});
+}
